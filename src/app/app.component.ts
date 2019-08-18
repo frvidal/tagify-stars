5cf41302a85bbb4f1086acb5d58a68f5106dd677
frvidal <frederic.vidal.perso@gmail.com>
2019-08-18 19:33:20 +0200

    [initial]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { TagStar } from './tag-star';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'tagify';
+export class AppComponent  {
+
+  title = 'Main container for tagify-stars';
+
+  private whitelist = ['java', 'javascript', 'C#', '.Net'];
+
+  private blacklist = [];
+
+  public originalValues = [{tag : 'java', star: 3}, {tag : '.Net', star: 0}];
+
+  onAddTagEvent(tagStar: TagStar) {
+    console.log ('Add event for ' + tagStar.tag + ' ' + tagStar.star);
+  }
+
+  onRemoveTagEvent(tag: string) {
+    console.log ('Remove event for ' + tag);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TagifyStarsComponent } from './tagify-stars/tagify-stars.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TagifyStarsComponent
   ],
   imports: [
     BrowserModule
